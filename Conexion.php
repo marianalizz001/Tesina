a3516f04cded1475b433b5a108603fe6d3da9692
ivtmarquez <57645487+ivtmarquez@users.noreply.github.com>
2020-11-19 20:32:31 -0600

    Cosas que no sirven

--- Conexion.php
+++ Conexion.php
@@ -1,8 +1,8 @@
 <?php
      require_once __DIR__ . "/vendor/autoload.php";
 
-     $conexion = new MongoDB\Client('mongodb+srv://Karen:Aloha98@cluster0-rzcxc.mongodb.net/Consultorio?retryWrites=true&w=majority');
-     $bd = $conexion->selectDatabase('Consultorio');
+     $conexion = new MongoDB\Client('mongodb+srv://Karen:Aloha98@cluster0-rzcxc.mongodb.net/Nutricion?retryWrites=true&w=majority');
+     $bd = $conexion->selectDatabase('Nutricion');
 ?>
 <?php
 /*
--- index.php
+++ index.php
@@ -1,60 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <link rel="icon" href="img/favicon.png">
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Anton|Dosis:400,800" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"/>
 
     <link rel="stylesheet" href="css/style.css">
-     
-    <title>Dra.YazminNajera | Home</title>
+    <link href="css/sweetalert.css" rel="stylesheet">
 
+    <title>NutriciónVida | Login</title>
 
-    <?php 
-    $_SESSION['log'] = false;
-    session_start();
-    session_destroy();
-    include("navbar.php"); ?>
+    <?php include("navbar.php"); ?>
     <br>
-  </head>
 
-  <body>
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="img/img7.png" alt="First slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/img2.jpg" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/img15.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/img1.jpg" alt="Third slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/img12.jpg" alt="Third slide">
+</head>
+<body>
+<section id="login" class="mb-5 mt-5">
+    <div class="container">
+      <br><br>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
+          <div class="card_login">
+            <div class="card">
+              <div class="card-body text-center">
+                <form action="loginPHP.php" method="post">
+                  <h2 class="h3 mb-3 font-weight-normal">Iniciar Sesión</h2><br>
+                  <div class="form-group">
+                      <input type="text" id="usuario" class="form-control" placeholder="Usuario" name="usuario" required>
+                  </div>
+                  <div class="form-group">
+                      <input type="password" id="clave" class="form-control" placeholder="Contraseña" name="clave" required>
+                  </div>
+                  <button type="submit" class="btn btn-lg btn-dark btn-block">Entrar</button>
+                </form>
+              </div>
+              <br><br>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+  </section>
+
 </body>
-  
 </html>
 <?php include("footer.php"); ?>
-

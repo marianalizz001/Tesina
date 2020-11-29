e09d504a64d8d43f6da26c83a312f39fe35857a8
ivtmarquez <57645487+ivtmarquez@users.noreply.github.com>
2020-11-28 20:35:44 -0600

    DISEÑO - LOGIN
    Login terminado

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
+<style>
+section,
+		html {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+			background: #82AC62 !important;
+		}
+		.user_card {
+			height: 400px;
+			width: 350px;
+			margin-top: auto;
+			margin-bottom: auto;
+			background: #f39c12;
+			position: relative;
+			display: flex;
+			justify-content: center;
+			flex-direction: column;
+			padding: 10px;
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			-webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			-moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			border-radius: 5px;
+
+		}
+		.brand_logo_container {
+			position: absolute;
+			height: 170px;
+			width: 170px;
+			top: -75px;
+			border-radius: 50%;
+			background: #82AC62;
+			padding: 10px;
+			text-align: center;
+		}
+		.brand_logo {
+			height: 150px;
+			width: 150px;
+			border-radius: 50%;
+		}
+		.form_container {
+			margin-top: 100px;
+		}
+		.login_btn {
+			width: 100%;
+			background: #c0392b !important;
+			color: white !important;
+		}
+		.login_btn:focus {
+			box-shadow: none !important;
+			outline: 0px !important;
+		}
+		.login_container {
+			padding: 0 2rem;
+		}
+		.input-group-text {
+			background: #c0392b !important;
+			color: white !important;
+			border: 0 !important;
+			border-radius: 0.25rem 0 0 0.25rem !important;
+		}
+		.input_user,
+		.input_pass:focus {
+			box-shadow: none !important;
+			outline: 0px !important;
+		}
+		.custom-checkbox .custom-control-input:checked~.custom-control-label::before {
+			background-color: #c0392b !important;
+		}
+</style>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,44 +80,56 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Anton|Dosis:400,800" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"/>
-
-    <link rel="stylesheet" href="css/style.css">
     <link href="css/sweetalert.css" rel="stylesheet">
 
-    <title>NutriciónVida | Login</title>
-
-    <?php include("navbar.php"); ?>
-    <br>
+    <title>Dra.YazminNajera | Login</title>
 
 </head>
-<body>
-<section id="login" class="mb-5 mt-5">
-    <div class="container">
-      <br><br>
-      <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
-          <div class="card_login">
-            <div class="card">
-              <div class="card-body text-center">
-                <form action="loginPHP.php" method="post">
-                  <h2 class="h3 mb-3 font-weight-normal">Iniciar Sesión</h2><br>
-                  <div class="form-group">
-                      <input type="text" id="usuario" class="form-control" placeholder="Usuario" name="usuario" required>
-                  </div>
-                  <div class="form-group">
-                      <input type="password" id="clave" class="form-control" placeholder="Contraseña" name="clave" required>
-                  </div>
-                  <button type="submit" class="btn btn-lg btn-dark btn-block">Entrar</button>
-                </form>
-              </div>
-              <br><br>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+<section id="login">
+<div class="container h-100">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="img/manzana.png" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				<div class="d-flex justify-content-center form_container">
+        <form action="loginPHP.php" method="post">
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text"  class="form-control input_user" id="usuario" placeholder="Usuario" name="usuario" required>
+						</div>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" id="clave" class="form-control input_pass" placeholder="Contraseña" name="clave" required>
+						</div>
+						<div class="form-group">
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customControlInline">
+								<label class="custom-control-label" for="customControlInline">Recuérdame</label>
+							</div>
+						</div>
+							<div class="d-flex justify-content-center mt-3 login_container">
+				 	<button type="submit" name="button" class="btn login_btn">Iniciar Sesión</button>
+				   </div>
+					</form>
+				</div>
+		
+				<div class="mt-4">
+					<div class="d-flex justify-content-center links">
+						¿No tienes una cuenta? <a href="#" class="ml-2">Crear</a>
+					</div>
+					<div class="d-flex justify-content-center links">
+						<a href="#">Olvidaste tu contraseña?</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
   </section>
-
-</body>
 </html>
-<?php include("footer.php"); ?>
